2b5c0d681561da152af533279c38d80b17ac872b
Jordan <jjohn468@uncc.edu>
2017-07-09 11:56:59 -0400

    Added a check to create the sum file the first time the app is run. Prevents running the app without being able to create the sum file

--- VoidCrypt/src/main.java
+++ VoidCrypt/src/main.java
@@ -1,9 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class main
 {
   public main() {}
   
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
+	if (isFirstRun()) {
+		if (setupSumFile()) {
+			JOptionPane.showMessageDialog(null,"An unknown error has occured setting up the application for first time use");
+			return;
+		}
+	}
     window w = new window();
+  }
+  private static boolean isFirstRun() {
+	  return !(new File("src/sums.info").exists());
+  }
+  private static boolean setupSumFile() throws Exception {
+	  File sumFile = new File("src/sums.info");
+	  if(!sumFile.exists()) {
+		  sumFile.createNewFile();
+	  }
+	  return isFirstRun();
   }
 }
